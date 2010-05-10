44b56f4a9b66de51e5ec3c232850b88a5f1f217f
Simon Nielsen <r.hackr@gmail.com>
2010-05-10 10:12:45 +0200

    fixed bug in locate that would that the wrong params

--- wrench.js
+++ wrench.js
@@ -1,9 +1,9 @@
-// By Simon Nielsen
+//By Simon Nielsen
 /*globals window document setInterval */
 "use strict";
 (function () {
   var wrench  = {},
-      routing = {routes: {}},
+      routing = {routes: {}, lastHash: "/"},
       events  = {},
       w       = window,
       d       = document;
@@ -30,10 +30,12 @@
   };  
       
   // ## Routing
-  // translate a route and some params into the window.location.hash
-  // builds a string for window.location.hash into multiple route partials if applicable
+  // translate a route and some params into the 
+  // window.location.hash builds a string for 
+  // window.location.hash into multiple route 
+  // partials if applicable
   routing.route2Hash = function (route, params, fromRoute) {
-		var hash  = route,
+    var hash  = route,
         i     = 1;
     if (typeof params !== 'undefined') {
       
@@ -73,15 +75,14 @@
 
   // find out if window.location.hash has changed
   // make sure this function can handle multiple calls
-  routing.lastHash = "/";
   routing.hashChanged = function () {
     var changed = w.location.hash != routing.lastHash && w.location.hash != "#" + routing.lastHash;
     return changed;
   };
   
-  // find out if a specific partial of window.location.hash has changed
+  // find out if a specific partial of 
+  // window.location.hash has changed
   routing.partialChanged = function (route) {
-    
     if (routing.hashChanged()) {
       var lastPartials    = routing.lastHash.replace("#", "").split(";"),
           currentPartials = w.location.hash.replace("#", "").split(";"),
@@ -106,21 +107,23 @@
     else { return false; }
   };
   
-  // looks at the current window.location.hash and routes to its function
-	// if one is registered and if the route has changed since last locate
-	// this is called onhashchange and on load of the page	
+  // looks at the current window.location.hash 
+  // and routes to its function if one is 
+  // registered and if the route has changed 
+  // since last locate this is called onhashchange 
+  // and on load of the page	
   routing.locate = function () {    
     if (routing.hashChanged()) {
-      var partials     = w.location.hash.replace("#", "").split(";"),
-          partialsLen  = partials.length,
-          params        = {}, 
-          route         = "";
+      var partials          = w.location.hash.replace("#", "").split(";"),
+          numberOfPartials  = partials.length;
 		
-      if (partialsLen > 0 && partials[0] !== "") {
+      if (numberOfPartials > 0 && partials[0] !== "") {
         
-        for (var i = 0; i < partialsLen; i++) {
+        for (var i = 0; i < numberOfPartials; i++) {
           var query   = partials[i].split("?"),
-              partial = query.shift();
+              partial = query.shift(),
+              params  = {},
+              route   = "";
               
           if (partial in routing.routes) {
             route = partial;
@@ -144,7 +147,6 @@
           }
                             
           if (route !== "" && routing.partialChanged(partial)) {  
-            
             if (route.indexOf("/") !== -1 && route.indexOf(":") !== -1) {
               var subRoutes   = route.split("/"),
                   subPartial  = partial.split("/");
@@ -178,23 +180,24 @@
   };
   
   // form elements on the page with '#' as the first
-  // character in the action attribute gets a onsubmit event added
-  // TODO: find a solution for live adding of onsubmit events if
-  // the user adds a form to the page after load.
+  // character in the action attribute gets a 
+  // onsubmit event added
   routing.attachFormRoutes = function () {    
+    var forms = d.forms;
     var handler = function (event) {
-      var form    = event.target,
-          params  = {};
-      for (var j = 0; j < form.elements.length; j++) {
-        params[form.elements[j].id] = form.elements[j].value;
+      var form      = event.target,
+          elements  = form.elements,
+          params    = {};
+      for (var j = 0; j < elements.length; j++) {
+        params[elements[j].id] = elements[j].value;
       }
       routing.route2Hash(form.action.replace("#", ""), params);
       event.preventDefault();
     };
     
-    for (var i = 0; i < d.forms.length; i++) {
-      if (d.forms[i].action.indexOf("#") === 0) {        
-        events.add(d.forms[i], "submit", handler);        
+    for (var i = 0; i < forms.length; i++) {
+      if (forms[i].action.indexOf("#") === 0) {        
+        events.add(forms[i], "submit", handler);        
       }
     }
   };
@@ -214,12 +217,14 @@
       // attach event listeners
       events.add(d, "DOMContentLoaded", bootstrap);
       
-      // fall back to calling the locate function every 250 miliseconds if the
-      // browser doesn't have the onhashchange event
+      // fall back to calling the locate function 
+      // every 250 miliseconds if the browser 
+      // doesn't have the onhashchange event
       if ("onhashchange" in w) { events.add(w, "hashchange", routing.locate); }
       else { setInterval(routing.locate, 200); }
       
-			// Use force loading if wrench is loaded way after the load event has triggered
+			// Use force loading if wrench is loaded way 
+			// after the load event has triggered
       if (force) { bootstrap(); }
 
       return app;
@@ -245,10 +250,11 @@
   };
 
   // connects a route to a function
-  // can be used like so: var users = route("users", function () {});
+  // can be used like so: 
+  // var users = route("users", function () {});
   // or: var users = route("users").to(function () {});
-  // all routed functions will be called with a params hash
-  // as the only argument		
+  // all routed functions will be called with a 
+  // params hash as the only argument		
   w.route = function (route, func) {
     var locate = function (params) {
       routing.route2Hash(route, params, true);
@@ -268,6 +274,5 @@
     }
   };
   
-	// add the global wrench variable
-  w.wrench = wrench;	
+  w.wrench = wrench;
 }());
